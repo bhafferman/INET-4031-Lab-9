23f04e27a85f4937f7a09bd23572e90b1e354922
Brandon Hafferman <bhafferman@icloud.com>
2024-04-11 07:56:07 +0000

    first commit

---
+++ bhaffe_inet4031_puppet_lab9/lamp_stack_server.pp
@@ -0,0 +1,47 @@
+package { 'apache2':
+  ensure => installed,
+}
+
+package { 'php':
+  ensure => installed,
+  notify  => Service['apache2'],
+  require => [Package['apache2']],
+}
+
+package { 'libapache2-mod-php':
+  ensure => installed,
+  require => Package['php'],
+}
+
+package { 'php-cli':
+  ensure => installed,
+  require => Package['php'],
+}
+
+package { 'php-mysql':
+  ensure => installed,
+  require => Package['php'],
+}
+
+package { 'mariadb-server':
+  ensure => installed,
+}
+
+file { '/var/www/html/phpinfo.php':
+  source => '/home/brandon/bhaffe_inet4031_puppet_lab9/phpinfo.php',
+  notify => Service['apache2'],
+  require => Package['apache2'],
+}
+
+service { 'apache2':
+  ensure => running,
+  enable => true,
+  require => [Package['apache2'], Package['php']]
+}
+
+service { 'mariadb':
+  ensure => running,
+  enable => true,
+  require => Package['mariadb-server'],
+}
+
